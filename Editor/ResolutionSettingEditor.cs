03d28d5360b6b9c9d3705645024898652ddadf62
Casey Hofland <hofland.casey@gmail.com>
2020-11-03 18:52:51 +0100

    ResolutionSetting
    This component does not work in the Editor. GameViewUtils has the ability to create custom screen sizes, however the implementation would need to find a clean way to keep this Editor functionality seperated from the Runtime script.

---
+++ Editor/ResolutionSettingEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+
+namespace UISettings.Editor
+{
+    [CustomEditor(typeof(ResolutionSetting))]
+    [CanEditMultipleObjects]
+    public class ResolutionSettingEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.HelpBox("Note that Resolution isn't updated in the Game View, only in builds.", MessageType.Info, true);
+        }
+    }
+}
+
---
+++ Runtime/ResolutionSetting.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UISettings
+{
+    public class ResolutionSetting : MonoBehaviour
+    {
+        #region Properties
+        [SerializeField] private Dropdown _dropdown;
+        [SerializeField] private bool _descending = false;
+
+        public Dropdown dropdown
+        {
+            get => _dropdown;
+            set
+            {
+                OnDisable();
+                _dropdown = value;
+                OnEnable();
+            }
+        }
+
+        public bool descending
+        {
+            get => _descending;
+            set
+            {
+                _descending = value;
+                SetOptions();
+            }
+        }
+        #endregion
+
+        #region Unity Methods
+        private void OnEnable()
+        {
+            dropdown.onValueChanged.AddListener(ValueChanged);
+
+            SetOptions();
+        }
+
+        private void OnDisable()
+        {
+            dropdown.onValueChanged.RemoveListener(ValueChanged);
+        }
+
+        private void Reset()
+        {
+            _dropdown = GetComponent<Dropdown>();
+        }
+        #endregion
+
+        #region Dropdown Handling
+        public void SetOptions()
+        {
+            dropdown.ClearOptions();
+
+            var options = new List<string>(Array.ConvertAll(Screen.resolutions, resolution => $"{resolution.width}x{resolution.height}"));
+            if(descending)
+            {
+                options.Reverse();
+            }
+
+            dropdown.AddOptions(options);
+
+            var resolutionIndex = Array.IndexOf(Screen.resolutions, Screen.currentResolution);
+            if(resolutionIndex != -1)
+            {
+                if(descending)
+                {
+                    resolutionIndex = Screen.resolutions.Length - 1 - resolutionIndex;
+                }
+
+                dropdown.SetValueWithoutNotify(resolutionIndex);
+                dropdown.RefreshShownValue();
+            }
+        }
+
+        private void ValueChanged(int value)
+        {
+            if(descending)
+            {
+                value = Screen.resolutions.Length - 1 - value;
+            }
+
+            var resolution = Screen.resolutions[value];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreenMode);
+        }
+        #endregion
+    }
+}

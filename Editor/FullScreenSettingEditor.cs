57fa89d6bad73ca65ee5f91d751b3f84d3d1ecbb
Casey Hofland <hofland.casey@gmail.com>
2020-11-03 18:54:00 +0100

    FullScreenSetting

---
+++ Editor/FullScreenSettingEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+
+namespace UISettings.Editor
+{
+    [CustomEditor(typeof(FullScreenSetting))]
+    [CanEditMultipleObjects]
+    public class FullScreenSettingEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            EditorGUILayout.HelpBox("Note that FullScreen isn't updated in the Game View, only in builds.", MessageType.Info, true);
+        }
+    }
+}
---
+++ Runtime/FullScreenSetting.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace UISettings
+{
+    public class FullScreenSetting : MonoBehaviour
+    {
+        #region Properties
+        [SerializeField] private Toggle _toggle;
+        [Tooltip("Display if the game is windowed instead of full screen.")] [SerializeField] private bool _windowed;
+
+        public Toggle toggle
+        {
+            get => _toggle;
+            set
+            {
+                OnDisable();
+                _toggle = value;
+                OnEnable();
+            }
+        }
+
+        public bool windowed
+        {
+            get => _windowed;
+            set
+            {
+                _windowed = value;
+                UpdateView();
+            }
+        }
+        #endregion
+
+        #region Unity Methods
+        private void OnEnable()
+        {
+            toggle.onValueChanged.AddListener(ValueChanged);
+
+            UpdateView();
+        }
+
+        private void OnDisable()
+        {
+            toggle.onValueChanged.RemoveListener(ValueChanged);
+        }
+
+        private void Reset()
+        {
+            _toggle = GetComponent<Toggle>();
+        }
+        #endregion
+
+        #region Toggle Handling
+        public void UpdateView()
+        {
+            toggle.SetIsOnWithoutNotify(Screen.fullScreen != windowed);
+        }
+
+        private void ValueChanged(bool value)
+        {
+            Screen.fullScreen = value != windowed;
+        }
+        #endregion
+    }
+}
+
